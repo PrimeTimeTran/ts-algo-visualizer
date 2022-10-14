067e32ca15f0777016b5805856484f6ad552ec71
Loi Van Tran <datloiboi@gmail.com>
2022-10-14 16:58:02 -0400

    Board working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// interface square {
+//   id: number,
+//   weight: 0,
+//   path: null,
+//   status: string,
+//   direction: null,
+//   previousNode: null,
+//   storedDirection: null,
+//   relatesToObject: false,
+//   heuristicDistance: null,
+//   distance: typeof Infinity,
+//   totalDistance: typeof Infinity,
+//   overwriteObjectRelation: false,
+//   // otherid: id,
+//   // otherstatus: status,
+//   // otherpreviousNode: null,
+//   // otherpath: null,
+//   // otherdirection: null,
+//   // otherstoredDirection: null,
+//   // otherdistance: Infinity,
+//   // otherweight: 0,
+//   // otherrelatesToObject: false,
+//   // otheroverwriteObjectRelation: false,
+// }
+
+type node = {
+  id: string;
+}
+
+function Game() {
+  var board: {}[][] = [];
+  for (var i = 0; i < 8; i++) {
+    board[i] = [];
+    for (var j = 0; j < 8; j++) {
+      const someObj: node = { id: `${i}, ${j}` };
+      board[i][j] = someObj;
+    }
+  }
+
+  console.log(board)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {board.map((row, i) => (
+            <div key={i}>
+              {row.map((col, j) => (
+                <span key={j}>{col}</span>
+              ))}
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
+}
+
+function App() {
+  return <div>
+    <Game />
+  </div>
 }
 
 export default App;
