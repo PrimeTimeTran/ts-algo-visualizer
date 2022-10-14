bd33d63b0e54c3d4b279320691c3a922990471cc
Loi Van Tran <datloiboi@gmail.com>
2022-10-14 19:53:05 -0400

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -27,41 +27,54 @@ import './App.css';
 //   // otheroverwriteObjectRelation: false,
 // }
 
+const ROWS = 10
+const COLS = 50
+
 type node = {
   id: string;
   row: number;
   col: number;
   checked: boolean;
+  start: boolean;
+  end: boolean;
 }
 
 type squareProps = {
   j: number;
   i: number;
   onClick: (arg1: number, arg2: number) => void;
-  col: {
+  square: {
     id: string;
     checked: boolean;
+    start: boolean;
+    end: boolean;
   };
 }
 
 function Square(props: squareProps) {
   return <span
-    key={props.j}
-    id={props.col.id}
+    key={props.square.id}
     onClick={() => props.onClick(props.i, props.j)}
-    className={`square ${props.col.checked ? 'checked' : ''}`}
+    className={`square ${props.square.checked ? 'checked' : ''} ${props.square.start ? 'start' : ''} ${props.square.end ? 'end' : ''}`}
   >
-    {props.col.id}
+    {props.square.id}
   </span>
 }
 
-
 function getBoard(shouldCheck = false) {
   var board: node[][] = [];
-  for (var i = 0; i < 8; i++) {
+  for (var i = 0; i < ROWS; i++) {
     board[i] = [];
-    for (var j = 0; j < 8; j++) {
-      const cur: node = { id: `${i},${j}`, checked: shouldCheck ? true : false, row: i, col: j };
+    for (var j = 0; j < COLS; j++) {
+      const rc = `${i},${j}`
+      const cur: node = {
+        row: i,
+        col: j,
+        id: rc,
+        checked: shouldCheck ? true : false,
+        start: rc === '5,5',
+        end: rc === '5,45',
+      };
       board[i][j] = cur;
     }
   }
@@ -70,36 +83,51 @@ function getBoard(shouldCheck = false) {
 
 function Game() {
   const [board, setBoard] = useState(getBoard())
-
   function onClick(r: number, c: number) {
-    board[r][c] = { ...board[r][c], checked: true }
-    const newBoard = [...board]
-    setBoard(newBoard)
+    var seen = new Set()
+    function dfs(r: number, c: number, delay: number) {
+      setTimeout(() => {
+        delay += 25
+        if (seen.has(`${r},${c}`) || r < 0 || c < 0 || r === ROWS || c === COLS) {
+          return
+        }
+        seen.add(`${r},${c}`);
+        board[r][c] = { ...board[r][c], checked: true }
+        const newBoard = [...board]
+        setBoard(newBoard)
+        dfs(r + 1, c, delay)
+        dfs(r, c + 1, delay)
+        dfs(r - 1, c, delay)
+        dfs(r, c - 1, delay)
+      }, delay);
+    }
+    let delay = 0
+    dfs(r, c, delay)
   }
 
-  function onDFS() {
-    let delay = 0
-    for (let r = 0; r < 8; r++) {
-      for (let c = 0; c < 8; c++) {
-        delay += 500
-        setTimeout(() => {
-          board[r][c] = { id: `${r},${c}`, checked: true, row: r, col: c }
-          setBoard([...board])
-        }, delay);
-      }
-    }
-  }
+  // function onDFS() {
+  //   let delay = 0
+  //   for (let r = 0; r < 8; r++) {
+  //     for (let c = 0; c < 8; c++) {
+  //       delay += 500
+  //       setTimeout(() => {
+  //         board[r][c] = { id: `${r},${c}`, checked: true, row: r, col: c }
+  //         setBoard([...board])
+  //       }, delay);
+  //     }
+  //   }
+  // }
 
   return (
     <div className="App">
-      <button onClick={onDFS}>DFS</button>
+      {/* <button onClick={onDFS}>DFS</button> */}
       <header className="App-header">
         <div>
           {board.map((row, i) => (
             <div key={i}>
               {row.map((col, j) => (
                 <Square
-                  col={col}
+                  square={col}
                   i={i}
                   j={j}
                   onClick={onClick}
