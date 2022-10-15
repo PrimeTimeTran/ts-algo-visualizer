811f3a50e375b537b06aa0613e466f62d2f40d86
Loi Van Tran <datloiboi@gmail.com>
2022-10-14 20:55:05 -0400

    WIP drawing line works

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { parse } from 'path';
 import { useState, useEffect } from 'react'
 
 import './App.css';
@@ -34,9 +35,11 @@ type node = {
   id: string;
   row: number;
   col: number;
-  checked: boolean;
-  start: boolean;
   end: boolean;
+  start: boolean;
+  checked: boolean;
+  inRoute: boolean;
+  distance: typeof Infinity;
 }
 
 type squareProps = {
@@ -45,24 +48,28 @@ type squareProps = {
   onClick: (arg1: number, arg2: number) => void;
   square: {
     id: string;
-    checked: boolean;
-    start: boolean;
     end: boolean;
+    start: boolean;
+    checked: boolean;
+    inRoute: boolean;
+    distance: typeof Infinity;
   };
 }
 
 function Square(props: squareProps) {
-  return <span
-    key={props.square.id}
-    onClick={() => props.onClick(props.i, props.j)}
-    className={`
+  const classes = `
       square 
       ${props.square.end ? 'end' : ''}
       ${props.square.start ? 'start' : ''} 
       ${props.square.checked ? 'checked' : ''} 
-    `}
+      ${props.square.inRoute ? 'inRoute' : ''} 
+    `
+  return <span
+    className={classes}
+    key={props.square.id}
+    id={props.square.id}
+    onClick={() => props.onClick(props.i, props.j)}
   >
-    {props.square.id}
   </span>
 }
 
@@ -76,9 +83,11 @@ function getBoard(shouldCheck = false) {
         row: i,
         col: j,
         id: rc,
-        checked: shouldCheck ? true : false,
-        start: rc === '5,5',
+        inRoute: false,
         end: rc === '5,45',
+        distance: Infinity,
+        start: rc === '5,5',
+        checked: shouldCheck ? true : false,
       };
       board[i][j] = cur;
     }
@@ -86,63 +95,83 @@ function getBoard(shouldCheck = false) {
   return board;
 }
 
+
+
 function Game() {
   const [board, setBoard] = useState(getBoard())
+  let foundit = false
+  function backTrack(start: string) {
+    setTimeout(() => {
+      if (foundit) return
+      const splitted = start.split(',')
+      var y = parseInt(splitted[0])
+      var x = parseInt(splitted[1])
+
+      if (y < 5) {
+        y++
+      } else if (y > 5) {
+        y--
+      } else if (x < 45) {
+        x++
+      } else if (x > 45) {
+        x--
+      } else if (y === 5 && x === 45) {
+        foundit = true
+        console.log('found it')
+      }
+      board[y][x].inRoute = true
+      setBoard([...board])
+      backTrack(`${y},${x}`)
+    }, 100);
+  }
+
   function onClick(r: number, c: number) {
     var seen = new Set()
     var stop = false
     var finishNode = '5,45'
-    function dfs(r: number, c: number, delay: number) {
+    var start = `${r},${c}`
+    function dfs(r: number, c: number, delay: number, distance: number) {
       setTimeout(() => {
         delay += 25
-        if (`${r},${c}` == finishNode) {
+        if (`${r},${c}` === finishNode) {
           stop = true
+          backTrack(start)
         }
         if (seen.has(`${r},${c}`) || r < 0 || c < 0 || r === ROWS || c === COLS || stop) {
           return
         }
         seen.add(`${r},${c}`);
-        board[r][c] = { ...board[r][c], checked: true }
+        board[r][c] = {
+          ...board[r][c],
+          checked: true,
+          start: start === `${r},${c}`,
+          distance: start === `${r},${c}` ? 0 : distance
+        }
         const newBoard = [...board]
         setBoard(newBoard)
-        dfs(r + 1, c, delay)
-        dfs(r, c + 1, delay)
-        dfs(r - 1, c, delay)
-        dfs(r, c - 1, delay)
+        dfs(r + 1, c, delay, distance + 1)
+        dfs(r, c + 1, delay, distance + 1)
+        dfs(r - 1, c, delay, distance + 1)
+        dfs(r, c - 1, delay, distance + 1)
       }, delay);
     }
     let delay = 0
-    dfs(r, c, delay)
+    dfs(r, c, delay, 0)
   }
-
-  // function onDFS() {
-  //   let delay = 0
-  //   for (let r = 0; r < 8; r++) {
-  //     for (let c = 0; c < 8; c++) {
-  //       delay += 500
-  //       setTimeout(() => {
-  //         board[r][c] = { id: `${r},${c}`, checked: true, row: r, col: c }
-  //         setBoard([...board])
-  //       }, delay);
-  //     }
-  //   }
-  // }
 
   return (
     <div className="App">
-      {/* <button onClick={onDFS}>DFS</button> */}
       <header className="App-header">
         <div>
           {board.map((row, i) => (
             <div key={i}>
               {row.map((col, j) => (
                 <Square
-                  square={col}
                   i={i}
                   j={j}
+                  square={col}
                   onClick={onClick}
                 />
-                // <span id={col.id} className={`square ${col.checked ? 'checked' : ''}`} onClick={() => onClick(i, j)} key={j}>{col.id}</span>
               ))}
             </div>
           ))}
